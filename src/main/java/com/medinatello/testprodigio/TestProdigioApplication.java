2cb5f703e33da9f9b8f429059bfe368976e24053
Jhoan Medina <medinatello@gmail.com>
2021-06-03 12:15:51 -0400

    Inicio

---
+++ src/main/java/com/medinatello/testprodigio/TestProdigioApplication.java
@@ -0,0 +1,13 @@
+package com.medinatello.testprodigio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProdigioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestProdigioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/medinatello/testprodigio/TestProdigioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinatello.testprodigio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProdigioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
